3fd58c20ae575e91bd116b69c9a5ba26126f905e
usuario <usuario@10.22.143.14>
2014-10-13 13:50:18 +0100

    Primeras clases implementadas.

--- src/histogram/HistogramBuilder.java
+++ src/histogram/HistogramBuilder.java
@@ -6,7 +6,15 @@ package histogram;
  */
 public class HistogramBuilder<Type> {
 
-    public HistogramBuilder<Type> build(Type[] types) {
-        return null;
+    public Histogram<Type> build(Type[] types) {
+        Histogram<Type> histogram = new Histogram<>();
+        for (Type type : types) {
+            if (histogram.containsKey(type)) {
+                histogram.put(type, histogram.get(type));
+            } else {
+                histogram.put(type, 1);
+            }
+        }
+        return histogram;
     }
 }
